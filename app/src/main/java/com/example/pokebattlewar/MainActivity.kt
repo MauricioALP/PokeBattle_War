7f861c3e8790db81f78e6010cb3f9008a3f36254
MauricioALP <t00049903@utbvirtual.edu.co>
2019-11-01 00:04:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pokebattlewar/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pokebattlewar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ButtonPlay.setOnClickListener {
+
+            val intent:Intent = Intent(this,Main2Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
